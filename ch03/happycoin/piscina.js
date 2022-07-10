a2aa50ec4ddeae3e28efc07e4c779710b3a64361
mercuriver <mercuriver@gmail.com>
2022-07-10 22:52:33 +0900

    feat(ch03): piscina sample #2, heap out of memory 발생

--- ch03/happycoin/piscina.js
+++ ch03/happycoin/piscina.js
@@ -1,10 +1,14 @@
 const Piscina = require("piscina");
+const assert = require("assert");
 
 if (!Piscina.isWorkerThread) {
   const piscina = new Piscina({ filename: __filename });
-  piscina.run(9).then((squareRootOfNine) => {
-    console.log("The square root of nine is", squareRootOfNine);
-  });
+
+  for (let i = 0; i < 10_000_000; i++) {
+    piscina.run(i).then((squareRootOfI) => {
+      assert.ok(typeof squareRootOfI === "number");
+    });
+  }
 }
 
 module.exports = (num) => Math.sqrt(num);
