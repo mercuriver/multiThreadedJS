b40010a7ab63644f9d489f646fd96784a32b55e5
mercuriver <mercuriver@gmail.com>
2022-07-10 22:56:00 +0900

    feat(ch03): piscina sample #3, 완료

--- ch03/happycoin/piscina.js
+++ ch03/happycoin/piscina.js
@@ -1,14 +1,20 @@
 const Piscina = require("piscina");
 const assert = require("assert");
+const { once } = require("events");
 
 if (!Piscina.isWorkerThread) {
-  const piscina = new Piscina({ filename: __filename });
+  const piscina = new Piscina({ filename: __filename, maxQueue: "auto" });
 
-  for (let i = 0; i < 10_000_000; i++) {
-    piscina.run(i).then((squareRootOfI) => {
-      assert.ok(typeof squareRootOfI === "number");
-    });
-  }
+  (async () => {
+    for (let i = 0; i < 10_000_000; i++) {
+      if (piscina.queueSize === piscina.options.maxQueue) {
+        await once(piscina, "drain");
+      }
+      piscina.run(i).then((squareRootOfI) => {
+        assert.ok(typeof squareRootOfI === "number");
+      });
+    }
+  })();
 }
 
 module.exports = (num) => Math.sqrt(num);
