29efa2959a61d7ca5801a6b08dcbeead5c98e534
mercuriver <mercuriver@gmail.com>
2022-07-10 20:48:29 +0900

    feat(ch03): MessageChannel: 데이터 스트림 구성

--- ch03/cluster/index.js
+++ ch03/cluster/index.js
@@ -1,26 +1,23 @@
-const http = require("http");
 const {
   Worker,
   isMainThread,
-  // workerData,
-  parentPort,
+  workerData,
+  MessageChannel,
 } = require("worker_threads");
 
 if (isMainThread) {
-  // const worker = new Worker(__filename, { workerData: { isWorker: true } });
-  const worker = new Worker(__filename);
-  worker.on("message", (msg) => {
-    worker.postMessage(msg);
+  const { port1, port2 } = new MessageChannel();
+  const worker = new Worker(__filename, {
+    workerData: { port: port2 },
+    transferList: [port2],
+  });
+  port1.on("message", (msg) => {
+    port1.postMessage(msg);
   });
 } else {
-  // http
-  //   .createServer((req, res) => {
-  //     console.log(workerData);
-  //     res.end("Hello, World\n");
-  //   })
-  //   .listen(3000);
-  parentPort.on("message", (msg) => {
+  const { port } = workerData;
+  port.on("message", (msg) => {
     console.log("We hot a message from the main thread:", msg);
   });
-  parentPort.postMessage("Hello, World!");
+  port.postMessage("Hello, World!");
 }
