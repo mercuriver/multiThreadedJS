c0f218d799ca6f2793a6d0f7947da1ea58dd9c8a
mercuriver <mercuriver@gmail.com>
2022-07-10 13:04:46 +0900

    feat(ch03): cluster 반영

--- ch03/cluster/index.js
+++ ch03/cluster/index.js
@@ -1,7 +1,15 @@
 const http = require("http");
+const cluster = require("cluster");
 
-http
-  .createServer((req, res) => {
-    res.end("Hello, World\n");
-  })
-  .listen(3000);
+if (cluster.isPrimary) {
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  http
+    .createServer((req, res) => {
+      res.end("Hello, World\n");
+    })
+    .listen(3000);
+}
