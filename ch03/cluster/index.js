c603227b05dae967aaa4d537cf7ab2d324a37db3
mercuriver <mercuriver@gmail.com>
2022-07-10 20:44:11 +0900

    feat(ch03): MessagePort: 데이터 스트림 구성

--- ch03/cluster/index.js
+++ ch03/cluster/index.js
@@ -1,13 +1,26 @@
 const http = require("http");
-const { Worker, isMainThread, workerData } = require("worker_threads");
+const {
+  Worker,
+  isMainThread,
+  // workerData,
+  parentPort,
+} = require("worker_threads");
 
 if (isMainThread) {
-  new Worker(__filename, { workerData: { isWorker: true } });
+  // const worker = new Worker(__filename, { workerData: { isWorker: true } });
+  const worker = new Worker(__filename);
+  worker.on("message", (msg) => {
+    worker.postMessage(msg);
+  });
 } else {
-  http
-    .createServer((req, res) => {
-      console.log(workerData);
-      res.end("Hello, World\n");
-    })
-    .listen(3000);
+  // http
+  //   .createServer((req, res) => {
+  //     console.log(workerData);
+  //     res.end("Hello, World\n");
+  //   })
+  //   .listen(3000);
+  parentPort.on("message", (msg) => {
+    console.log("We hot a message from the main thread:", msg);
+  });
+  parentPort.postMessage("Hello, World!");
 }
