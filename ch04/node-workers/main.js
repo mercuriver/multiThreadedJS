756d550c6a14e7e8a35f274be3bce161c9e4f4b3
mercuriver <mercuriver@gmail.com>
2022-07-12 22:02:32 +0900

    feat(ch04): SharedArrayBuffer 공유 메모리 예제 #2

---
+++ ch04/node-workers/main.js
@@ -0,0 +1,18 @@
+#!/user/bin/env node
+
+const { Worker } = require("worker_threads");
+
+const worker = new Worker(__dirname + "/worker.js");
+
+const buffer = new SharedArrayBuffer(1024);
+const view = new Uint8Array(buffer);
+
+console.log("now", view[0]);
+
+worker.postMessage(buffer);
+
+setTimeout(() => {
+  console.log("later", view[0]);
+  console.log("props", buffer.foo);
+  worker.unref();
+}, 500);
