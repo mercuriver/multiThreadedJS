913f41865762ea00d80b5cf8722ed5f470ecc893
mercuriver <mercuriver@gmail.com>
2022-07-23 15:21:41 +0900

    feat(ch05): Multi Thread 콘웨이 생명 게임 #comp, 코디네이터 처리부 구현

--- ch05/thread-game-of-life/gol.js
+++ ch05/thread-game-of-life/gol.js
@@ -128,6 +128,26 @@ if (isMainThread) {
     }
   }
 
+  function runCoord() {
+    for (let i = 0; i < THREADS; i++) {
+      Atomics.store(sync, i, 1);
+      Atomics.notify(sync, i);
+    }
+    for (let i = 0; i < THREADS; i++) {
+      Atomics.wait(sync, i, 1);
+    }
+    const oldCells = cells;
+    cells = nextCells;
+    nextCells = oldCells;
+
+    for (let x = 0; x < SIZE; x++) {
+      for (let y = 0; y < SIZE; y++) {
+        sharedImageBuf[SIZE * x + y] = cells[SIZE * x + y] ? BLACK : WHITE;
+      }
+    }
+    self.postMessage({});
+  }
+
   function initListener(msg) {
     const opts = msg.data;
     sharedMemory = opts.sharedMemory;
