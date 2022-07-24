5507fd02f0351fa0c507b467937a0ef5e3f071f2
mercuriver <mercuriver@gmail.com>
2022-07-24 13:38:18 +0900

    feat(ch06): RingBuffer #comp, 생성자-소비자 패턴 예제 구성

--- ch06/ring-buffer/ring-buffer.js
+++ ch06/ring-buffer/ring-buffer.js
@@ -105,3 +105,31 @@ class SharedRingBuffer {
     return this.lock.exec(() => this.ringBuffer.read(bytes));
   }
 }
+
+const { isMainThread, Worker, workerData } = require("worker_threads");
+const fs = require("fs");
+
+if (isMainThread) {
+  const shared = new SharedArrayBuffer(116);
+  const threads = [
+    new Worker(__filename, { workerData: { shared, isProducer: true } }),
+    new Worker(__filename, { workerData: { shared, isProducer: true } }),
+    new Worker(__filename, { workerData: { shared, isProducer: false } }),
+    new Worker(__filename, { workerData: { shared, isProducer: false } }),
+  ];
+} else {
+  const { shared, isProducer } = workerData;
+  const ringBuffer = new SharedRingBuffer(shared);
+
+  if (isProducer) {
+    const buffer = Buffer.from("Hello, World\n");
+    while (true) {
+      ringBuffer.write(buffer);
+    }
+  } else {
+    while (true) {
+      const readBytes = ringBuffer.read(20);
+      fs.writeSync(1, `Read ${readBytes.length} bytes\n`);
+    }
+  }
+}
