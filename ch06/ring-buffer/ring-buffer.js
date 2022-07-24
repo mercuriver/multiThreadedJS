9310bee4524eedc4a5d802791fcde3d65f68695c
mercuriver <mercuriver@gmail.com>
2022-07-24 13:29:47 +0900

    feat(ch06): RingBuffer #2, RingBuffer 클래스 작성 완료

--- ch06/ring-buffer/ring-buffer.js
+++ ch06/ring-buffer/ring-buffer.js
@@ -57,4 +57,29 @@ class RingBuffer {
     this.length += bytesWritten;
     return bytesWritten;
   }
+
+  read(bytes) {
+    if (bytes > this.length) {
+      bytes = this.length;
+    }
+    if (bytes === 0) {
+      return new Uint8Array(0);
+    }
+    let readData;
+    if (this.head > this.tail || this.buffer.length - this.tail >= bytes) {
+      readData = this.buffer.slice(this.tail, bytes);
+      this.tail += bytes;
+    } else {
+      readData = new Uint8Array(bytes);
+      const endBytesToRead = this.buffer.length - this.tail;
+      readData.set(this.buffer.subarray(this.tail, this.buffer.length));
+      readData.set(
+        this.buffer.subarray(0, bytes - endBytesToRead),
+        endBytesToRead
+      );
+      this.tail = bytes - endBytesToRead;
+    }
+    this.length -= bytes;
+    return readData;
+  }
 }
