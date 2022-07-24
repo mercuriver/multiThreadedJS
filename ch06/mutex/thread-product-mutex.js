2c56646d1f2613994fab61fca43d0e2546fda28c
mercuriver <mercuriver@gmail.com>
2022-07-24 12:30:20 +0900

    feat(ch06): Mutex 예제 오류 수정

--- ch06/mutex/thread-product-mutex.js
+++ ch06/mutex/thread-product-mutex.js
@@ -1,12 +1,12 @@
 const { Worker, isMainThread, workerData } = require("worker_threads");
 const assert = require("assert");
+
 const Mutex = require("./mutex.js");
 
 if (isMainThread) {
-  const shared = new SharedArrayBuffer(4 * 4);
+  const shared = new SharedArrayBuffer(4 * 5);
   const sharedInts = new Int32Array(shared);
-
-  sharedInts.set([2, 3, 5, 7]);
+  sharedInts.set([2, 3, 5, 7, 0]);
   for (let i = 0; i < 3; i++) {
     new Worker(__filename, { workerData: { i, shared } });
   }
@@ -16,11 +16,11 @@ if (isMainThread) {
   const mutex = new Mutex(sharedInts, 4);
 
   mutex.exec(() => {
-    const a = Atomics.load(sharedInts, i);
+    const a = sharedInts[i];
     for (let j = 0; j < 1_000_000; j++) {} // 의도적 지연 발생
-    const b = Atomics.load(sharedInts, 3);
-    Atomics.store(sharedInts, 3, a * b);
-    assert.strictEqual(Atomics.load(sharedInts, 3), a * b);
+    const b = sharedInts[3];
+    sharedInts[3] = a * b;
+    assert.strictEqual(sharedInts[3], a * b);
   });
 }
 // 다른 스레드에 의해 공유 버퍼 값이 갱신되는 상황을 의도
