183996f6116cb703e4ea74a449f738a5985b2402
mercuriver <mercuriver@gmail.com>
2022-07-24 13:33:08 +0900

    feat(ch06): RingBuffer #3, Mutex를 적용한 SharedRingBuffer 클래스 작성

--- ch06/ring-buffer/ring-buffer.js
+++ ch06/ring-buffer/ring-buffer.js
@@ -83,3 +83,25 @@ class RingBuffer {
     return readData;
   }
 }
+
+const Mutex = require("../mutex/mutex.js");
+
+class SharedRingBuffer {
+  constructor(shared /*: number | SharedArrayBuffer */) {
+    this.shared =
+      typeof shared === "number" ? new SharedArrayBuffer(shared + 16) : shared;
+    this.ringBuffer = new RingBuffer(
+      new Uint32Array(this.shared, 4, 3),
+      new Uint8Array(this.shared, 16)
+    );
+    this.lock = new Mutex(new Int32Array(this.shared, 0, 1));
+  }
+
+  write(data) {
+    return this.lock.exec(() => this.ringBuffer.write(data));
+  }
+
+  read(bytes) {
+    return this.lock.exec(() => this.ringBuffer.read(bytes));
+  }
+}
