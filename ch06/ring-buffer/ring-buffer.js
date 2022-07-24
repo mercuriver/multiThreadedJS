ada541f6fe7e2035bb0664219718c0dca0beba76
mercuriver <mercuriver@gmail.com>
2022-07-24 13:23:04 +0900

    feat(ch06): RingBuffer, 클래스 작성 #1

---
+++ ch06/ring-buffer/ring-buffer.js
@@ -0,0 +1,60 @@
+class RingBuffer {
+  constructor(meta /*: Uint32Array[3] */, buffer /*: Uint8Array */) {
+    this.meta = meta; // [head, tail, lenght]
+    this.buffer = buffer;
+  }
+
+  get head() {
+    return this.meta[0];
+  }
+  set head(n) {
+    this.meta[0] = n;
+  }
+
+  get tail() {
+    return this.meta[1];
+  }
+  set tail(n) {
+    this.meta[1] = n;
+  }
+
+  get length() {
+    return this.meta[2];
+  }
+  set length(n) {
+    this.meta[2] = n;
+  }
+
+  write(data /*: Uint8Array */) {
+    let bytesWritten = data.length;
+    if (bytesWritten > this.buffer.length - this.length) {
+      bytesWritten = this.buffer.length - this.length;
+      data = data.subarray(0, bytesWritten);
+    }
+
+    if (bytesWritten === 0) {
+      return bytesWritten;
+    }
+
+    if (
+      (this.head >= this.tail &&
+        this.buffer.length - this.head >= bytesWritten) ||
+      (this.head < this.tail && bytesWritten <= this.tail - this.head)
+    ) {
+      this.buffer.set(data, this.head);
+      this.head += bytesWritten;
+    } else {
+      // 데이터를 쓰기에 충분한 버퍼 사이즈가 확보되지 않은 경우
+      // 데이터를 2개의 chunk로 나누어 버퍼에 입력
+      const endSpaceAvailable = this.buffer.length - this.head;
+      const endChunk = data.subarray(0, endSpaceAvailable);
+      const beginChunk = data.subarray(endSpaceAvailable);
+      this.buffer.set(endChunk, this.head);
+      this.buffer.set(beginChunk, 0);
+      this.head = beginChunk.length;
+    }
+
+    this.length += bytesWritten;
+    return bytesWritten;
+  }
+}
