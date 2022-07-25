8d67f58a9feba988b3f870d9903eeaf29d1b8955
mercuriver <mercuriver@gmail.com>
2022-07-25 22:21:38 +0900

    feat(ch08): template render #1, server 기본 리스너 등록

---
+++ ch08/template-render/server.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const Fastify = require("fastify");
+const RpcWorkerPool = require("./rpc-worker.js");
+const worker = new RpcWorkerPool("./worker.js", 4, "leastBusy");
+const template = require("./template.js");
+const server = Fastify();
+
+server.get("/main", async (request, reply) =>
+  template.renderLove({ me: "Thomas", you: "katelyn" })
+);
+
+server.get("/offload", async (request, reply) =>
+  worker.exec("renderLove", { me: "Thomas", you: "katelyn" })
+);
+
+server.listen(3000, (err, address) => {
+  if (err) throw err;
+  console.log(`listening on: ${address}`);
+});
